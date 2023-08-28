9ce78255dd531f21510c090a536cfa2dee295f3e
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-28 18:30:01 +0300

    feat: remove context, useLocalstorage, css vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
-import { ThemeProvider } from "./providers";
 import { Header, Main, Footer } from "./components";
 
 function App() {
   return (
-    <ThemeProvider>
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
-    </ThemeProvider>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
